5b7b5cc659bc393208ac5c61414ebbdabb566db4
ACSC CyberLab <cyber.lab@defence.gov.au>
2017-03-07 04:18:29 +1100

    Added prometheus-style metrics for monitoring

--- reststore/webapp.py
+++ reststore/webapp.py
@@ -12,7 +12,11 @@ except ImportError:
     from http import client
 
 import bottle
+from bottle import response
 bottle.BaseRequest.MEMFILE_MAX = 1600000000
+
+import prometheus_client
+from prometheus_client import Gauge, Summary
 
 import reststore
 from reststore import config
@@ -20,8 +24,38 @@ from reststore import config
 proxy_requests = False
 
 
+# prometheus metrics state
+request_summary = Summary(
+    'reststore_api_request_duration_seconds',
+    'Time spent processing api request',
+    ['resource', 'method']
+)
+request_timer = lambda *x: request_summary.labels(*x).time()
+
+file_count_gauge = Gauge(
+    'reststore_stored_files',
+    'Number of files in reststore',
+    ['store']
+)
+file_count_gauge._samples = lambda: _counts()
+
+file_size_summary = Summary(
+    'reststore_file_size_bytes',
+    'Size of files stored/fetched in bytes',
+    ['store', 'direction']
+)
+
+# unfortunately do not have a way to query for current
+# filestores, so we accumulate them as requests are seen
+known_file_stores = set()
+
+def _counts():
+    return [('', {'store': k}, len(_get_files(k))) \
+        for k in known_file_stores]
+
 def _get_files(name):
     global proxy_requests
+    known_file_stores.add(name)
     if proxy_requests:
         files = reststore.FilesClient(name=name)
     else:
@@ -60,12 +94,15 @@ def wrap_json_error(f):
         
 @bottle.get('/<name>/file/<hexdigest>')
 @wrap_json_error
+@request_timer('/store/file', 'get')
 def get(name, hexdigest):
     files = _get_files(name)
     try:
         filepath = files[hexdigest]
         with open(filepath) as f:
-            data = base64.encodestring(zlib.compress(f.read()))
+            data = f.read()
+            file_size_summary.labels(name, 'download').observe(len(data))
+            data = base64.encodestring(zlib.compress(data))
     except KeyError:
         raise JSONError(client.NOT_FOUND, 
                 exception='KeyError',
@@ -75,10 +112,12 @@ def get(name, hexdigest):
 
 @bottle.put('/<name>/file/<hexdigest>')
 @wrap_json_error
+@request_timer('/store/file', 'put')
 def put(name, hexdigest):
     files = _get_files(name)
     data = bottle.request.body.read()
     data = zlib.decompress(base64.decodestring(data))
+    file_size_summary.labels(name, 'upload').observe(len(data))
     try:
         files.put(data, hexdigest=hexdigest)
     except ValueError as err:
@@ -90,6 +129,7 @@ def put(name, hexdigest):
 
 @bottle.post('/<name>/file')
 @wrap_json_error
+@request_timer('/store/file', 'post')
 def post_multiple_files(name):
     """Multiple file post
 
@@ -101,6 +141,7 @@ def post_multiple_files(name):
         insert_files = body['files']
         for hexdigest, data in insert_files:
             data = zlib.decompress(base64.decodestring(data))
+            file_size_summary.labels(name, 'upload').observe(len(data))
             files.put(data, hexdigest=hexdigest)
     except ValueError as err:
         raise JSONError(client.BAD_REQUEST,
@@ -111,6 +152,7 @@ def post_multiple_files(name):
 
 @bottle.get('/<name>/length')
 @wrap_json_error
+@request_timer('/store/length', 'get')
 def get_length(name):
     files = _get_files(name)
     return dict(result=len(files))
@@ -118,6 +160,7 @@ def get_length(name):
 
 @bottle.get('/<name>/select/<a>/<b>')
 @wrap_json_error
+@request_timer('/store/select', 'get')
 def get_select(name, a, b):
     files = _get_files(name)
     hexdigests = files.select(int(a), int(b))
@@ -125,10 +168,18 @@ def get_select(name, a, b):
 
 
 @bottle.get('/<name>/contains/<hexdigest>')
+@request_timer('/store/contains', 'get')
 @wrap_json_error
 def contains(name, hexdigest):
     files = _get_files(name)
     return dict(result=hexdigest in files)
+
+@bottle.get('/metrics')
+@request_timer('/metrics', 'get')
+def prometheus_metrics():
+    """Prometheus exporter api"""
+    response.content_type = prometheus_client.CONTENT_TYPE_LATEST
+    return prometheus_client.generate_latest()
 
 
 app = bottle.default_app()
@@ -142,6 +193,4 @@ def run():
                        server=webapp_config['server'])
     proxy_requests = config.values['webapp']['proxy_requests']
     bottle.run(app=app, **bottle_kwargs)
-
-
 
